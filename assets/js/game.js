aa9e0173195212d52aa8e4c06af01849e0ab3b80
William Saunders <willjsaunders@icloud.com>
2024-05-17 20:16:50 +0000

    fix: get try...catch block working by dereferencing json file and creating array of objects for backup pokemon.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,262 @@
-const backupPokemon = require("./backupPokemon.json");
+const backupPokemon = [
+    {
+        id: 1,
+        name: "Bulbasaur",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "grass",
+                },
+            },
+        ],
+    },
+    {
+        id: 4,
+        name: "Charmander",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "fire",
+                },
+            },
+        ],
+    },
+    {
+        id: 775,
+        name: "Komala",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/775.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "normal",
+                },
+            },
+        ],
+    },
+    {
+        id: 25,
+        name: "Pikachu",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "electric",
+                },
+            },
+        ],
+    },
+    {
+        id: 59,
+        name: "Arcanine",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "fire",
+                },
+            },
+        ],
+    },
+    {
+        id: 79,
+        name: "Slowbro",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/79.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "water",
+                },
+            },
+        ],
+    },
+    {
+        id: 111,
+        name: "Rhyhorn",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/111.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "ground",
+                },
+            },
+        ],
+    },
+    {
+        id: 142,
+        name: "Snorlax",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/142.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "normal",
+                },
+            },
+        ],
+    },
+    {
+        id: 149,
+        name: "Dragonite",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "dragon",
+                },
+            },
+        ],
+    },
+    {
+        id: 152,
+        name: "Bayleef",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "grass",
+                },
+            },
+        ],
+    },
+    {
+        id: 175,
+        name: "Togepi",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/175.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "fairy",
+                },
+            },
+        ],
+    },
+    {
+        id: 197,
+        name: "Umbreon",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/197.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "dark",
+                },
+            },
+        ],
+    },
+    {
+        id: 267,
+        name: "Beautifly",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/267.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "bug",
+                },
+            },
+        ],
+    },
+    {
+        id: 296,
+        name: "Makuhita",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/296.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "fighting",
+                },
+            },
+        ],
+    },
+    {
+        id: 480,
+        name: "Uxie",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/480.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "psychic",
+                },
+            },
+        ],
+    },
+    {
+        id: 583,
+        name: "Vanillish",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/583.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "ice",
+                },
+            },
+        ],
+    },
+    {
+        id: 600,
+        name: "Klang",
+        sprites: {
+            front_default:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/600.png",
+        },
+        types: [
+            {
+                type: {
+                    name: "steel",
+                },
+            },
+        ],
+    },
+];
 
-const pokeBaseURL = "https://pokeapi.co/api/v2/pokemon/";
+const pokeBaseURL = "https://pokeapi.co/api/v8/pokemon/";
 const game = document.getElementById("game");
 const btn = document.getElementById("btn");
 const scoreElement = document.getElementById("score");
@@ -74,6 +330,11 @@ const loadPokemon = async (numberOfPairs) => {
         }
         const pokePromises = [...randIds].map((id) => fetch(pokeBaseURL + id)); // Spread the randIds set into an array which supports .map. For each id in the array, fetch a promise from the base URL with random id appended. Returns an array of promises
         const results = await Promise.all(pokePromises); // returns a single promise that resolves when all pokePromises have resolved; returns an array
+        for (let result of results) {
+            if (!result.ok) {
+                throw new Error("Pokemon failed with error");
+            }
+        }
         return await Promise.all(results.map((res) => res.json()));
     } catch {
         return backupPokemon.slice(0, numberOfPairs);
