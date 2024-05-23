86c01f0391814c1c39cb3f98def42b06bc4b5aa5
William Saunders <willjsaunders@icloud.com>
2024-05-23 18:51:03 +0000

    chore: fix scoreboard h2 and move backupPokemon to separate file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,267 +1,11 @@
-const backupPokemon = [
-    {
-        id: 1,
-        name: "Bulbasaur",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "grass",
-                },
-            },
-        ],
-    },
-    {
-        id: 4,
-        name: "Charmander",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "fire",
-                },
-            },
-        ],
-    },
-    {
-        id: 775,
-        name: "Komala",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/775.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "normal",
-                },
-            },
-        ],
-    },
-    {
-        id: 25,
-        name: "Pikachu",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "electric",
-                },
-            },
-        ],
-    },
-    {
-        id: 59,
-        name: "Arcanine",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "fire",
-                },
-            },
-        ],
-    },
-    {
-        id: 79,
-        name: "Slowbro",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/79.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "water",
-                },
-            },
-        ],
-    },
-    {
-        id: 111,
-        name: "Rhyhorn",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/111.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "ground",
-                },
-            },
-        ],
-    },
-    {
-        id: 142,
-        name: "Snorlax",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/142.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "normal",
-                },
-            },
-        ],
-    },
-    {
-        id: 149,
-        name: "Dragonite",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "dragon",
-                },
-            },
-        ],
-    },
-    {
-        id: 152,
-        name: "Bayleef",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "grass",
-                },
-            },
-        ],
-    },
-    {
-        id: 175,
-        name: "Togepi",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/175.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "fairy",
-                },
-            },
-        ],
-    },
-    {
-        id: 197,
-        name: "Umbreon",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/197.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "dark",
-                },
-            },
-        ],
-    },
-    {
-        id: 267,
-        name: "Beautifly",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/267.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "bug",
-                },
-            },
-        ],
-    },
-    {
-        id: 296,
-        name: "Makuhita",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/296.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "fighting",
-                },
-            },
-        ],
-    },
-    {
-        id: 480,
-        name: "Uxie",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/480.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "psychic",
-                },
-            },
-        ],
-    },
-    {
-        id: 583,
-        name: "Vanillish",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/583.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "ice",
-                },
-            },
-        ],
-    },
-    {
-        id: 600,
-        name: "Klang",
-        sprites: {
-            front_default:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/600.png",
-        },
-        types: [
-            {
-                type: {
-                    name: "steel",
-                },
-            },
-        ],
-    },
-];
-
-const pokeBaseURL = "https://pokeapi.co/api/v2/pokemon/";
+import { backupPokemon } from "./backupPokemon.js";
+const pokeBaseURL = "https://pokeapi.co/api/v8/pokemon/";
 const game = document.getElementById("game");
 const btn = document.getElementById("btn");
 const scoreElement = document.getElementById("score");
 const dropdown = document.getElementById("dropdown");
-// listen for change in number of pairs
+
+// listen for change in number of pairs and update variable accordingly
 dropdown.addEventListener("change", function () {
     numberOfPairs = parseInt(this.value);
 });
@@ -371,6 +115,7 @@ const createCard = (pokemon) => {
     const backImage = document.createElement("img");
     backImage.src =
         "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00749e58-41ca-4e6e-add6-55da22501c91/dexc4ag-47c47f39-89a4-477e-919c-f13d72286a64.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwNzQ5ZTU4LTQxY2EtNGU2ZS1hZGQ2LTU1ZGEyMjUwMWM5MVwvZGV4YzRhZy00N2M0N2YzOS04OWE0LTQ3N2UtOTE5Yy1mMTNkNzIyODZhNjQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.OHUH-qup0p6ki77yTrbOcet5UrnBXLDSZ67SoahcC8Q";
+
     cardBack.appendChild(backImage);
 
     // add unique id for card div
@@ -562,9 +307,12 @@ const showHighScore = () => {
     let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
     let highScoreDiv = document.getElementById("high-scores");
     highScoreDiv.innerHTML = "";
+    let html = "<h2>High Scores</h2><ol>";
     for (i = 0; i < highScores.length; i++) {
-        highScoreDiv.innerHTML += `<li>${highScores[i]}</li>`;
+        html += `<li>${highScores[i]}</li>`;
     }
+    html += "</ol>";
+    highScoreDiv.innerHTML = html;
 };
 
 // Call this function so that 'new game' buttom resets the game without having to refresh page
