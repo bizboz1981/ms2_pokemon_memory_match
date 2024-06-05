2fc09fd95d3449c141cdb570e2ee411f1bf83877
William Saunders <willjsaunders@icloud.com>
2024-06-05 08:53:14 +0000

    chore: textResize bottom

--- assets/js/game.js
+++ assets/js/game.js
@@ -125,7 +125,7 @@ const resizeText = () => {
         while (text.offsetWidth > cardSize) {
             fontSize--;
             text.style.fontSize = `${fontSize}px`;
-            text.style.bottom = "-10px";
+            text.style.bottom = "-13px";
             // Trigger a reflow by getting DOM property
             text.offsetHeight;  
         }
